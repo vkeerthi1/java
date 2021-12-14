34895908a1e54a4b947a06a56542d015acae69c3
keerthi v <91740419+vkeerthi1@users.noreply.github.com>
2021-12-14 18:12:06 +0530

    Update Shape.java

--- JAVA9_ABOVE/SEALED/Shape.java
+++ JAVA9_ABOVE/SEALED/Shape.java
@@ -1,3 +1,9 @@
+#Sealed classes and interfaces restrict which other classes or interfaces may extend or implement them.
+
+#To seal a class, add the sealed modifier to its declaration. Then, after any extends and implements clauses, add the permits clause. This clause specifies the classes that may extend the sealed class. 
+#For example, the following declaration of Shape specifies three permitted subclasses, Circle, Square, and Rectangle: 
+
+
 public sealed class Shape permits Circle, Square, Rectangle {
 }
 
