4e5dda5d2774825f945170ed4640ddef75b12a95
keerthi v <91740419+vkeerthi1@users.noreply.github.com>
2021-12-14 18:11:16 +0530

    Update Shape.java

--- JAVA9_ABOVE/SEALED/Shape.java
+++ JAVA9_ABOVE/SEALED/Shape.java
@@ -1,2 +1,13 @@
 public sealed class Shape permits Circle, Square, Rectangle {
 }
+
+final class Circle extends Shape {
+    public float radius;
+}
+non-sealed class Square extends Shape {
+   public double side;
+}   
+sealed class Rectangle extends Shape permits FilledRectangle {
+    public double length, width;
+}
+
