1995cbc6afcce721998f65925b5384b96a2c053f
keerthi v <91740419+vkeerthi1@users.noreply.github.com>
2022-01-19 17:21:04 +0530

    Create AutoBoxing.java

---
+++ Advanced/AutoBoxing.java
@@ -0,0 +1,31 @@
+package com.java;
+
+public class AutoBoxing {
+
+    public static void main(String args[]) {
+
+        // Example 1: == comparison pure primitive – no autoboxing
+        int i1 = 1;
+        int i2 = 1;
+        System.out.println("i1==i2 : " + (i1 == i2)); // true
+
+        // Example 2: equality operator mixing object and primitive
+        Integer num1 = 1; // autoboxing
+        int num2 = 1;
+        System.out.println("num1 == num2 : " + (num1 == num2)); // true
+
+        // Example 3: special case - arises due to autoboxing in Java
+        Integer obj1 = 1; // autoboxing will call Integer.valueOf()
+        Integer obj2 = 1; // same call to Integer.valueOf() will return same
+        // cached Object
+
+        System.out.println("obj1 == obj2 : " + (obj1 == obj2)); // true
+
+        // Example 4: equality operator - pure object comparison
+        Integer one = new Integer(1); // no autoboxing
+        Integer anotherOne = new Integer(1);
+        System.out.println("one == anotherOne : " + (one == anotherOne)); // false
+
+    }
+
+}
