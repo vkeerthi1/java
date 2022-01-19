2145a6c315e53bddc56609919dca44c273c9ea43
keerthi v <91740419+vkeerthi1@users.noreply.github.com>
2022-01-19 17:27:48 +0530

    Add files via upload

---
+++ Advanced/Completable Future/src/main/java/com/home/async/SupplyAsyncDemo.java
@@ -0,0 +1,30 @@
+package com.home.async;
+
+import com.home.async.database.EmployeeDatabase;
+import com.home.async.dto.Employee;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class SupplyAsyncDemo {
+
+    public List<Employee> getEmployees() throws ExecutionException, InterruptedException {
+        Executor executor = Executors.newCachedThreadPool();
+        CompletableFuture<List<Employee>> listCompletableFuture = CompletableFuture
+                .supplyAsync(() -> {
+                    System.out.println("Executed by : " + Thread.currentThread().getName());
+                    return EmployeeDatabase.fetchEmployees();
+                },executor);
+        return listCompletableFuture.get();
+    }
+
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        SupplyAsyncDemo supplyAsyncDemo = new SupplyAsyncDemo();
+        List<Employee> employees = supplyAsyncDemo.getEmployees();
+        employees.stream().forEach(System.out::println);
+    }
+}
